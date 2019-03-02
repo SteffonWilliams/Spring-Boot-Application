b6163b5d963cb08039719d014a64ae0485a9ec24
Steffon Williams <steffonwilliams@Steffons-MacBook-Air.local>
2019-03-02 18:25:19 -0500

    implemented Post Request

--- src/main/java/steffon/io/springboot/topic/TopicService.java
+++ src/main/java/steffon/io/springboot/topic/TopicService.java
@@ -2,16 +2,17 @@ package steffon.io.springboot.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-   private List<Topic> topics = Arrays.asList(
+   private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Descripition" ),
                 new Topic("java", "Core Java", "Core Java Descripition" ),
                 new Topic("javascript", "JavaScript", "JavaScript Description")
-        );
+        ));
 
 
    public List<Topic> getAllTopics(){
@@ -21,4 +22,8 @@ public class TopicService {
    public Topic getTopic(String id){
       return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
    }
+
+    public void addTopic(Topic topic) {
+       topics.add(topic);
+    }
 }
