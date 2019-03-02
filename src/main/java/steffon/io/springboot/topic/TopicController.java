da49ed515a9ad48f55e08e246f123ce93bdb1bf9
Steffon Williams <steffonwilliams@Steffons-MacBook-Air.local>
2019-03-02 18:49:25 -0500

    Topic Controller Complete

--- src/main/java/steffon/io/springboot/topic/TopicController.java
+++ src/main/java/steffon/io/springboot/topic/TopicController.java
@@ -30,4 +30,16 @@ public class TopicController {
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+
+        topicService.updateTopic(id,topic);
+
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic( @PathVariable String id){
+        topicService.deleteTopic(id);
+
+    }
+
 }
