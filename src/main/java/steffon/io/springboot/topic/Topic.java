7a8b6d2b8ea07d5d7e7b72df450ee24dab8b2414
Steffon Williams <steffonwilliams@Steffons-MacBook-Air.local>
2019-03-02 15:31:13 -0500

    added TopicController

--- src/main/java/steffon/io/springboot/topic/Topic.java
+++ src/main/java/steffon/io/springboot/topic/Topic.java
@@ -6,6 +6,15 @@ public class Topic {
     private String name;
     private String description;
 
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     public String getId() {
         return id;
     }
--- src/main/java/steffon/io/springboot/topic/TopicController.java
+++ src/main/java/steffon/io/springboot/topic/TopicController.java
@@ -3,11 +3,18 @@ package steffon.io.springboot.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics(){
-        return "All Topics";
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Descripition" ),
+                new Topic("java", "Core Java", "Core Java Descripition" ),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+        );
     }
 }
